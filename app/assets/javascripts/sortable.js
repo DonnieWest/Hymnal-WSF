8a613d8554080f54925f6a58024b40cbfb93d8f9
Jared L Jennings <jared@jaredjennings.org>
2014-09-26 21:54:00 -0500

    Bottom navbar

---
+++ app/assets/javascripts/sortable.js
@@ -0,0 +1,12 @@
+/**
+ * Created by jjennings on 9/26/14.
+ */
+
+//This code makes the th tag clickable and looks at the data-link tag we set for the url
+$(document).ready(function(){
+
+    $('.multiSort').click(function(){
+        window.location.href = $(this).data('link');
+    });
+
+});
