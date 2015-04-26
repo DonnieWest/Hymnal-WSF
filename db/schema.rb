848e355816073db5e25dcf55a1f79668c6dcdf6d
Jared Jennings <jared@jaredjennings.org>
2015-04-26 17:51:46 -0500

    Add Password to User Modal

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150422030518) do
+ActiveRecord::Schema.define(version: 20150426225112) do
 
   create_table "bootsy_image_galleries", force: :cascade do |t|
     t.integer  "bootsy_resource_id"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20150422030518) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "password"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
